bdafdb3e25dff05d0b32de57758dc9af44648e06
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2021-10-13 22:34:11 -0300

    optimization

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
@@ -17,7 +17,9 @@ export function SideBar({
 }: SideBarProps) {
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
         {genres.map(genre => (
@@ -30,7 +32,6 @@ export function SideBar({
           />
         ))}
       </div>
-
     </nav>
-  )
+  );
 }
